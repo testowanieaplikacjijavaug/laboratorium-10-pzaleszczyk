4ee68ab65d46f243b7140b178e8cfdccfeeb098d
Suchy-Laptop <Suchy-Laptop@LAPTOP-BMKRSQB4>
2020-04-27 15:41:45 +0200

    headless firefox

--- src/test/java/FirefoxTest.java
+++ src/test/java/FirefoxTest.java
@@ -10,18 +10,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
-public class GoogleTest {
+public class FirefoxTest {
 	
 	private static WebDriver driver;
 
 	@BeforeAll
 	public static void setUpDriver(){
-		System.setProperty("webdriver.gecko.driver", "resources/geckodriver");
-		driver = new FirefoxDriver();
-		// Implicity wait -> max czas na znalezienie elementu na stronie
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setHeadless(true);
+        driver = new FirefoxDriver(firefoxOptions);
+        // Implicity wait -> max czas na znalezienie elementu na stronie
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@BeforeEach
