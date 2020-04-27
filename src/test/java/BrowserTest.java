e58d1862892d82fc1bf6f23be5be31969e6370e1
Pawel <moonster1j@gmail.com>
2020-04-27 21:12:37 +0200

    Fixes

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -8,18 +8,20 @@ import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserTest {
-	
+
 	private static WebDriver driver;
 
 	@BeforeAll
@@ -31,39 +33,57 @@ public class BrowserTest {
 		}
 		else
 			System.setProperty("webdriver.gecko.driver", "resources/geckodriver");
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.setHeadless(true);
-        driver = new FirefoxDriver(firefoxOptions);
-        // Implicity wait -> max czas na znalezienie elementu na stronie
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		FirefoxOptions firefoxOptions = new FirefoxOptions();
+		firefoxOptions.setHeadless(true);
+		driver = new FirefoxDriver(firefoxOptions);
+		// Implicity wait -> max czas na znalezienie elementu na stronie
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@BeforeEach
 	public void setUp() throws Exception {
-    	driver.get("https://duckduckgo.com/");
+		driver.get("https://duckduckgo.com/");
 	}
 
 	@AfterAll
 	public static void tearDown() throws Exception {
 		driver.quit();
 	}
-	
+
 	@Test
 	public void findFirst() {
 		driver.findElement(By.id("search_form_input_homepage")).sendKeys("mfi.ug.edu.pl");
-        driver.findElement(By.id("search_button_homepage")).click();
-        driver.findElement(By.id("r1-0")).click();
-        assertEquals("https://mfi.ug.edu.pl/", driver.getCurrentUrl());
+		driver.findElement(By.id("search_button_homepage")).click();
+		driver.findElement(By.id("r1-0")).click();
+		assertEquals("https://mfi.ug.edu.pl/", driver.getCurrentUrl());
 	}
-	
+
 	@Test
 	public void findThird() {
 		driver.findElement(By.id("search_form_input_homepage")).sendKeys("mfi.ug.edu.pl");
-        driver.findElement(By.id("search_button_homepage")).click();
-        driver.findElement(By.id("r1-2")).click();
-        assertEquals("https://mat.ug.edu.pl/pracownicy/dr-maciej-mroczkowski/", driver.getCurrentUrl());
+		driver.findElement(By.id("search_button_homepage")).click();
+		driver.findElement(By.id("r1-2")).click();
+		assertEquals("https://en.mfi.ug.edu.pl/students/erasmus_0", driver.getCurrentUrl());
 	}
 
+
+	@Test
+	public void noClick() {
+		driver.findElement(By.id("search_form_input_homepage")).sendKeys("mfi.ug.edu.pl");
+		driver.findElement(By.id("search_button_homepage")).sendKeys(Keys.RETURN);
+		String name = driver.findElement(By.id("r1-0")).getText().substring(0,41);
+		assertEquals("Wydział Matematyki, Fizyki i Informatyki", name);
+	}
+
+	@Test
+	public void noResult() {
+		Assertions.assertThrows(NoSuchElementException.class, () -> {
+			driver.findElement(By.id("search_form_input_homepage")).sendKeys("//!?@!?#!?");
+			driver.findElement(By.id("search_button_homepage")).click();
+			driver.findElement(By.id("r1-0")).click();
+			assertEquals("https://mfi.ug.edu.pl/", driver.getCurrentUrl());
+		});
+	}
 
 
 }
