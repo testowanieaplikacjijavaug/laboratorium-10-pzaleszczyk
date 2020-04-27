3b644982633cbae41573b82b958905245f8f5b17
Pawel <moonster1j@gmail.com>
2020-04-27 23:09:19 +0200

    'z3'

--- src/test/java/XPath3Test.java
+++ src/test/java/XPath3Test.java
@@ -2,6 +2,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterAll;
@@ -10,11 +11,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class XPath3Test {
-	
+
 	private static WebDriver driver;
 
 	@BeforeAll
@@ -26,11 +28,11 @@ public class XPath3Test {
 		}
 		else
 			System.setProperty("webdriver.gecko.driver", "resources/geckodriver");
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.setHeadless(true);
-        driver = new FirefoxDriver(firefoxOptions);
-        // Implicity wait -> max czas na znalezienie elementu na stronie
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		FirefoxOptions firefoxOptions = new FirefoxOptions();
+		firefoxOptions.setHeadless(true);
+		driver = new FirefoxDriver(firefoxOptions);
+		// Implicity wait -> max czas na znalezienie elementu na stronie
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@BeforeEach
@@ -44,4 +46,59 @@ public class XPath3Test {
 	}
 
 
+
+	@Test
+	public void countImg() {
+		List<WebElement> elements = driver.findElements(By.xpath("//img"));
+		int amount = 0;
+		for ( WebElement element : elements ) {
+			amount++;
+		}
+		assertEquals(23, amount);
+	}
+
+	@Test
+	public void countHrefs() {
+		List<WebElement> elements = driver.findElements(By.xpath("//a"));
+		int amount = 0;
+		for ( WebElement element : elements ) {
+			if(element.getAttribute("href") != null)
+				amount++;
+		}
+		assertEquals(82, amount);
+	}
+	
+	@Test
+	public void enterHrefs() {
+		//*[@id="content"]/div[3]/div[1]/a
+		List<WebElement> elements = driver.findElements(By.xpath("//a"));
+		int visited = 0;
+		for ( WebElement element : elements ) {
+			String href = element.getAttribute("href");
+			if(href != null) {
+				if(!href.startsWith("/")) {
+					element.getLocation();
+					driver.navigate().back();
+				}
+				else {
+					driver.navigate().to(driver.getCurrentUrl()+href);
+					driver.navigate().back();
+				}
+			}
+		}
+		assertEquals("https://inf.ug.edu.pl/studinfo",driver.getCurrentUrl());
+	}
+	
+	@Test
+	public void enterInputs() {
+		driver.navigate().to("https://inf.ug.edu.pl/sq/src/login.php");
+		
+		List<WebElement> elements = driver.findElements(By.xpath("//input")); 
+		int count = 0;
+		for ( WebElement element : elements ) {
+			if(element.getAttribute("value").length() > 0)
+				count++;
+		}
+		assertEquals(3, count);
+	}
 }
